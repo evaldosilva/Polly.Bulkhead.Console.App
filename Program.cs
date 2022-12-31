68c415ed75cdefa185a885133a7214136e30f4bd
Evaldo Gomes Silva <evaldogomes@gmail.com>
2022-12-31 16:01:46 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Polly;
+
+Console.WriteLine("--- Polly .Net test BULKHEAD ---");
+
+var bulkheadPolicy = Policy.BulkheadAsync(1, 1, async (Context) => {
+    await Task.Run(() =>
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("I could not handle this request.");
+    }); 
+});
+
+while (true)
+{
+    Task.Delay(1000).Wait();
+    bulkheadPolicy.ExecuteAsync(DoThisTask);
+}
+
+static Task DoThisTask()
+{
+    return Task.Run(() => {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Do some task.");
+        Task.Delay(5000).Wait();
+    });
+}
